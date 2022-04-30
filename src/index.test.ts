802b814a59adba98d8b68997e90038f4779eb28a
srbitgithub <sr_bit@hotmail.com>
2022-04-30 07:42:03 +0200

    Check if received data is an X

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,13 +2,24 @@ import { BowlingGame } from "./index"
 
 describe("BowlingGame Score", () => {
 
-  const bowlingGame:BowlingGame = new BowlingGame()
-  
-  it("returns 'BowlingGame works fine' to test BowlingGame Class", () => {
+  it('returns "BowlingGame works fine" to test BowlingGame Class', () => {
+    const bowlingGame:BowlingGame = new BowlingGame()
+
     const expected = "BowlingGame works fine";
 
     const result = bowlingGame.result;
 
     expect(result).toEqual(expected);
   })
+
+  it('returns true if receive an X', () => {
+    const bowlingGame:BowlingGame = new BowlingGame('X')
+
+    const expected = true;
+
+    const result = bowlingGame.isStrike;
+
+    expect(result).toEqual(expected);
+  })
+
 })
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
 export class BowlingGame {
+
+    dataReceived:string = '' 
+    isStrike:boolean = false;
+
+    constructor(_dataReceived?:string ){
+
+      if (_dataReceived != null) {
+        this.dataReceived = _dataReceived
+      }
+
+      if (this.dataReceived === 'X') {
+        this.isStrike = true
+      }
+
+    }
+
+    
+    
     result:string = "BowlingGame works fine"
   };
